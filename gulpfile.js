dae33a8e966351ff605c6740d2c0ab5438b555b5
Dmitriy Ovsyannikov <ovsyannikov.dmitry@gmail.com>
2019-01-03 18:22:38 +0500

    Finish build

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,8 @@ let rename = require("gulp-rename");
 let del = require("del");
 let imagemin = require("gulp-imagemin");
 let svgstore = require("gulp-svgstore");
+let posthtml = require("gulp-posthtml");
+let include = require("posthtml-include");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -37,7 +39,7 @@ gulp.task("images", function() {
 })
 
 gulp.task("sprite", function() {
-  return gulp.src("source/img/*icon-*.svg")
+  return gulp.src("source/img/icon-*.svg")
   .pipe(svgstore({
     inlineSvg: true
   }))
@@ -45,9 +47,17 @@ gulp.task("sprite", function() {
   .pipe(gulp.dest("build/img"))
 })
 
+gulp.task("html", function() {
+  return gulp.src("source/*.html")
+  .pipe(posthtml([
+    include()
+  ]))
+  .pipe(gulp.dest("build"))
+})
+
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -55,8 +65,14 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
+
+gulp.task("refresh", function(done) {
+  server.reload();
+  done();
+})
 
 gulp.task("copy", function() {
   return gulp.src([
@@ -71,4 +87,5 @@ gulp.task("clean", function() {
   return del("build");
 })
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("start", gulp.series("build", "server"));
